6e9e172297ca916e020a0ee0927c7685af9081b6
康良玉 <kangly@vigamemedia.com>
2020-11-26 11:49:57 +0800

    Hello World

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * @Author 康良玉
+ * @Description 描述
+ * @Create 2020-11-25 14:53
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
