aae1248c1cfb97dee9ad8c04f2f2180f34cf7927
康良玉 <kangly@vigamemedia.com>
2020-11-26 11:52:18 +0800

    单链表的两种翻转操作

---
+++ src/com/caogen/algorithm/study4/test2.java
@@ -0,0 +1,61 @@
+package com.caogen.algorithm.study4;
+
+import com.caogen.algorithm.common.Node;
+import com.caogen.algorithm.common.NodeUtil;
+
+/**
+ * @Author 康良玉
+ * @Description 描述
+ * @Create 2020-11-25 16:37
+ */
+public class test2 {
+
+    public static void main(String[] args) {
+        //构建单链表
+        Node node5 = new Node(5, null);
+        Node node4 = new Node(4, node5);
+        Node node3 = new Node(3, node4);
+        Node node2 = new Node(2, node3);
+        Node head = new Node(1, node2);
+
+        NodeUtil.printnewHead(head);
+        System.out.println();
+
+        Node reverse = reverse(head);
+        NodeUtil.printnewHead(reverse);
+        System.out.println();
+
+        Node reverse2 = reverse(reverse);
+        NodeUtil.printnewHead(reverse2);
+        System.out.println();
+
+    }
+
+    public static Node reverse(Node head) {
+        //翻转
+        Node curr = head.getNext();
+        Node prev = head;
+        Node next;
+        while (curr != null) {
+            next = curr.getNext();
+            curr.setNext(prev);
+            prev = curr;
+            curr = next;
+        }
+        head.setNext(null);
+
+        return prev;
+    }
+
+    public static Node reverse2(Node head) {
+        if(head == null || head.getNext() ==null) {
+            return head;
+        }
+
+        Node reverse = reverse2(head.getNext());
+        head.getNext().setNext(head);
+        head.setNext(null);
+
+        return reverse;
+    }
+}
