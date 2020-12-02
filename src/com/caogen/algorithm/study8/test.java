9dc64a709f7a32688d9a6b2eb233be6c7e0c8d81
康良玉 <kangly@vigamemedia.com>
2020-12-02 14:47:25 +0800

    用栈实现字符串中的单词翻转

---
+++ src/com/caogen/algorithm/study8/test.java
@@ -0,0 +1,37 @@
+package com.caogen.algorithm.study8;
+
+import java.util.Stack;
+
+/**
+ * @Author 康良玉
+ * @Description 描述
+ * 最后我们给出一道练习题。给定一个字符串，逐个翻转字符串中的每个单词。例如，输入: "the sky is blue"，输出: "blue is sky the"
+ * @Create 2020-12-02 14:36
+ */
+public class test {
+
+    public static void main(String[] args) {
+        String str = "the sky is blue";
+        test(str);
+    }
+
+    public static void test(String str) {
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            char c = str.charAt(i);
+            if (!String.valueOf(c).equals(" ")) {
+                stack.add(c);
+            } else {
+                while (!stack.empty()) {
+                    System.out.print(stack.pop());
+                }
+                System.out.print(" ");
+            }
+        }
+
+        while (!stack.empty()) {
+            System.out.print(stack.pop());
+        }
+    }
+
+}
