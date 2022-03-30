90ffafc6d8574291fb64a56c299a3ecb6a86db68
康良玉 <kangly@vigamemedia.com>
2022-03-30 11:57:34 +0800

    加入一个分数字段,重写toString()和compareTo()方法

--- src/com/caogen/algorithm/imooc/common/Student.java
+++ src/com/caogen/algorithm/imooc/common/Student.java
@@ -7,15 +7,18 @@ import java.util.Objects;
  * @Description 描述
  * @Create 2022-03-25 17:04
  */
-public class Student {
+public class Student implements Comparable<Student> {
 
     private Integer id;
 
     private String name;
 
-    public Student(Integer id, String name) {
+    private int score;
+
+    public Student(Integer id, String name, int score) {
         this.id = id;
         this.name = name;
+        this.score = score;
     }
 
     public Integer getId() {
@@ -34,6 +37,14 @@ public class Student {
         this.name = name;
     }
 
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -44,11 +55,31 @@ public class Student {
 
         }
         Student student = (Student) o;
-        return Objects.equals(getId(), student.getId()) && Objects.equals(getName(), student.getName());
+        return Objects.equals(getId(), student.getId()) && Objects.equals(getName(), student.getName()) && Objects.equals(getScore(), student.getScore());
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(getId(), getName());
+    }
+
+
+    @Override
+    public int compareTo(Student o) {
+        if (this.getScore() < o.getScore()) {
+            return -1;
+        } else if (this.getScore() == o.getScore()) {
+            return 0;
+        }
+        return 1;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", score=" + score +
+                '}';
     }
 }
