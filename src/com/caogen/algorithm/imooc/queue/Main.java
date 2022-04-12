ab6b88c17200fba659c27f8ce8cddfd47340d097
康良玉 <kangly@vigamemedia.com>
2022-04-12 17:11:51 +0800

    循环队列和数组队列性能测试

--- src/com/caogen/algorithm/imooc/queue/Main.java
+++ src/com/caogen/algorithm/imooc/queue/Main.java
@@ -1,5 +1,7 @@
 package com.caogen.algorithm.imooc.queue;
 
+import java.util.Random;
+
 /**
  * @Author 康良玉
  * @Description 描述
@@ -7,16 +9,32 @@ package com.caogen.algorithm.imooc.queue;
  */
 public class Main {
 
-    public static void main(String[] args) {
-        ArrayQueue<Integer> queue = new ArrayQueue<>();
+    private static double testQueue(Queue<Integer> queue, int opCount) {
+        long startTime = System.nanoTime();
 
-        for (int i = 0; i < 5; i++) {
-            queue.enqueue(i);
-            System.out.println(queue);
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) {
+            queue.enqueue(random.nextInt(Integer.MAX_VALUE));
         }
+        for (int i = 0; i < opCount; i++) {
+            queue.dequeue();
+        }
+        long endTime = System.nanoTime();
 
-        queue.dequeue();
-        System.out.println(queue);
+        double time = (endTime - startTime) / 1000000000.0;
+        return time;
+    }
+
+    public static void main(String[] args) {
+        int opCount = 100000;
+
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+        double time1 = testQueue(arrayQueue, opCount);
+        System.out.println("ArrayQueue, time: " + time1 + " s");
+
+        LoopQueue<Integer> loopQueue = new LoopQueue<>();
+        double time2 = testQueue(loopQueue, opCount);
+        System.out.println("LoopQueue, time: " + time2 + " s");
     }
 
 }
